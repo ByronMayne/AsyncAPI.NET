cb935fea23bc97fe6c09131583c7eb5185d84171
Byron Mayne <Byronmayne@gmail.com>
2024-04-06 12:26:03 -0400

    Added IsExternalInit to allow init properties

---
+++ src/LEGO.AsyncAPI/Properties/IsExternalInit.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Class required to be define to use init properties.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit
+    {
+    }
+}
